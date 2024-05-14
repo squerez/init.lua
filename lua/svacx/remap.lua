97cee8702b145ddd1d6cf36b010c50f9ff7d0ecd
joasan2 <joasan2@emea.corpdir.net>
2024-05-14 13:28:28 +0100

    updated config

--- lua/svacx/remap.lua
+++ lua/svacx/remap.lua
@@ -12,10 +12,10 @@ vim.keymap.set("n", "N", "Nzzzv")
 
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
@@ -43,3 +43,7 @@ vim.keymap.set('n', '<leader>B', function() require('dap').set_breakpoint(vim.fn
 
 vim.keymap.set('n', '<leader>cc', ':Copilot enable<CR>')
 vim.keymap.set('n', '<leader>ccd', ':Copilot disable<CR>')
+
+vim.keymap.set('n', '<leader>w', ':%s/\\s\\+$//e<CR>', { silent = true, noremap = true })
+vim.keymap.set('n', '<leader>t', ':!pipenv run pytest % -vvv<CR>', { silent = true, noremap = true })
+vim.keymap.set('n', '<leader>fl', ':!pipenv run flake8 % <CR>', { silent = true, noremap = true })
